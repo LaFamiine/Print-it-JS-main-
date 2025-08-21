0448ddd9904fb5d96dbae39f6047496212125cea
Anastasia said <anastasiasaidfara@gmail.com>
2025-08-22 00:31:52 +0200

    ajout evenement

---
+++ assets/script.js
@@ -0,0 +1,40 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+//variable
+let compteur = 0;
+
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const bannerImg = document.querySelector('.banner-img');
+const tagLine = document.querySelector('#banner p');
+//fonction
+function changeImage() {
+	bannerImg.src = `assets/images/slideshow/${slides[compteur].image}`;
+}
+
+//evenement
+arrowLeft.addEventListener('click', function() {
+	compteur--;
+	changeImage();
+});
+
+arrowRight.addEventListener('click', function() {
+	compteur++;
+	changeImage();
+});
